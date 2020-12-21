43910561c4977b677211a8462c69db0489f474ed
sanket karvekar <sksanket7@gmail.com>
2020-12-20 20:36:29 -0800

    Added via direct upload

---
+++ src/com/sanket/collection/ComparabaleComparator.java
@@ -0,0 +1,21 @@
+package com.sanket.collection;
+
+import java.util.Comparator;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class ComparabaleComparator implements Comparator<Integer> {
+    public static void main(String[] args) {
+        Set<Integer> treeSet=new TreeSet<Integer>();
+        treeSet.add(7);
+        treeSet.add(1);
+        treeSet.add(2);
+        treeSet.add(9);
+        System.out.println(treeSet.toString());
+    }
+
+    @Override
+    public int compare(Integer o1, Integer o2) {
+        return 0;
+    }
+}
